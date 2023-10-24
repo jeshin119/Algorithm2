d9de154f54224aec2d5212cd93b0d92049998600
Jeong-su Shin <jeshin@c6r5s8.42seoul.kr>
2023-10-24 22:08:26 +0900

    5_N added

--- 5_N.cpp
+++ 5_N.cpp
@@ -1,14 +1,43 @@
+// BOJ url ; https://www.acmicpc.net/problem/3190
 #include <bits/stdc++.h>
 using namespace std;
-int n;
-int a[24][24];
+int n,k,l,ret,cnt;
+pair<int,int>apples;
+pair<int,int>turns;
+int a[104][104];
+int snake[104][104];
+int	go(int y,int x){
+	if(y<0||x<0||y>=n||x>=n)
+		continue;
+	for(int turn:turns){
+		if(cnt ==turn){
+			if(turn.second =='L')
+
+			else if(turn.seconde =='D')
+		}
+	}
+	for(int ap:apples){
+		if(y==ap.first && x ==ap.second){
+			
+		}
+	}
+	cnt++;
+}
+
 int main()
 {
 	cin>>n;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			cin >>a[i][j];
-		}
+	cin>>k;
+	for(int i=0;i<k;i++){
+		cin>>apples[i].fisrt>>apples[i].second;
+	}
+	cin>>l;
+	for(int i=0;i<l,i++){
+		cin>>turns[i].first>>turns[i].second;
+	}
+	while(true){
+		int y=0;int x=0;
+		
 	}
 	
 }
